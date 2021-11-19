d1830afae26ad4d8614f525d61b79e4cebcc99e4
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-11-19 11:47:27 +0000

    Game module constructor method and active player function

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 class Game {
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
     
+    
+    /** 
+     * Returns active player.
+     * @return  {Object}    player - The active player.
+     */
+	get activePlayer() {
+        return this.players.find(player => player.active);
+	}
 }

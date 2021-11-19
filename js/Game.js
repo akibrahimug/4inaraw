abb59a6da46a8e0546ada9de04fd159bbd28604a
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-11-19 11:49:07 +0000

    create players function

--- js/Game.js
+++ js/Game.js
@@ -13,4 +13,16 @@ class Game {
 	get activePlayer() {
         return this.players.find(player => player.active);
 	}
+
+       /** 
+     * Creates two player objects
+     * @return  {array}    An array of two player objects.
+     */
+        createPlayers() {
+            const players = [new Player('Player 1', 1, '#e15258', true),
+                             new Player('Player 2', 2, '#e59a13')];
+            return players;
+        }
+        
 }
+

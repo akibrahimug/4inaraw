bb6d65c51816e4a74c47788a4c3bbc478946a2cf
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-12-08 16:19:01 +0000

    Player/space and token functionality

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,31 @@
 class Player {
-    
+    constructor(name, id, tokenColor, active = false){
+        this.name = name;
+        this.id = id;
+        this.tokenColor = tokenColor;
+        this.active = active;
+        this.tokens = this.createTokens(21)
+    }
+    /**
+     * Creates token object for player
+     * @param {integer} num - Number of token objects to be created 
+     * 
+     */
+    createTokens(num){
+        const tokens = [];
+
+        for(let i = 0; i < num; i++){
+            let token = new Token(i, this);
+            tokens.push(token);
+        }
+
+        return tokens;
+    }
+/**
+ * Gets all tokens that havent been dropped.
+ * @return {array} Array of unused tokens
+ */
+    get unusedTokens(){
+        return this.tokens.filter(token => !token.dropped);
+    }
 }

1e5b0d6a775ca4f8a48c850fd64faef922b07977
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-12-13 00:32:39 +0000

    ds-files

--- js/Player.js
+++ js/Player.js
@@ -28,4 +28,21 @@ class Player {
     get unusedTokens(){
         return this.tokens.filter(token => !token.dropped);
     }
+
+       /**
+     * Gets the active token by returning the first token in the array of unused tokens.
+     * @return {Object} First token object in the array of unused tokens.
+     */
+	get activeToken() {
+        return this.unusedTokens[0];
+    }
+    
+
+    /**
+     * Check if a player has any undropped tokens left
+     * @return {Boolean} 
+     */
+    checkTokens(){
+        return this.unusedTokens.length == 0 ? false : true;
+    }
 }
